a977dd48e9049cf9a3399ead78874c0135c9d1ab
Phillip Whelan <pwhelan@mixxx.org>
2016-12-23 17:16:36 -0300

    Update Multipart.php
    FIX: typo that basically makes passing $content do the exact opposite of what it should.

--- src/p3k/Multipart.php
+++ src/p3k/Multipart.php
@@ -44,7 +44,7 @@ class Multipart {
     $this->_data .= 'Content-Type: ' . $type . self::EOL;
     $this->_data .= 'Content-Transfer-Encoding: binary' . self::EOL;
     $this->_data .= self::EOL;
-    if ($content) $this->_data .= file_get_contents($filename) . self::EOL;
+    if (!$content) $this->_data .= file_get_contents($filename) . self::EOL;
     else $this->_data .= $content . self::EOL;
   }
 
